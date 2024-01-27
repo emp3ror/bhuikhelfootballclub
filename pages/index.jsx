7f6d0d4c18c8a2f889cfe82919b5417bfc2d437f
mjt <thapa.manish16@gmail.com>
2024-01-27 19:28:17 +0545

    fb chat on site

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { getPosts } from '../utils/mdx-utils';
 
+import FBChat from '../components/FacebooChat';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Layout, { GradientBackground } from '../components/Layout';
@@ -13,7 +14,7 @@ export default function Index({ posts, globalData }) {
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
-      
+      <FBChat/>
       
       <main className="w-full pt-20 pb-12">
         <h1 className="text-3xl lg:text-5xl text-center mb-12 sentence">
